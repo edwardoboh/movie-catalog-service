f6b779d47e4228667f750d80344f17e5c225e2a8
edwardoboh <obohedward@gmail.com>
2024-01-23 07:57:52 +0100

    chore: add eureka client configuration

--- src/main/java/com/edwardoboh/moviemicro/moviecatalogservice/controllers/MovieCatalogController.java
+++ src/main/java/com/edwardoboh/moviemicro/moviecatalogservice/controllers/MovieCatalogController.java
@@ -2,8 +2,11 @@ package com.edwardoboh.moviemicro.moviecatalogservice.controllers;
 
 import com.edwardoboh.moviemicro.moviecatalogservice.model.MovieCatalogEntity;
 import com.edwardoboh.moviemicro.moviecatalogservice.model.MovieDetailEntity;
+import com.edwardoboh.moviemicro.moviecatalogservice.model.MovieRatingEntity;
 import com.edwardoboh.moviemicro.moviecatalogservice.model.UserMovieRatingEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -22,38 +25,35 @@ public class MovieCatalogController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Autowired
     WebClient.Builder webclientBuilder;
 
-    /*
     @GetMapping("/{userId}")
     public ResponseEntity<List<MovieCatalogEntity>> fetchCatalog(@PathVariable("userId") String userId) {
         List<MovieRatingEntity> movieRatingList;
         try{
-            ResponseEntity<MovieRatingEntity[]> responseEntity =
-                    restTemplate.exchange(
-                            "http://localhost:3003/rating/" + userId,
-                            HttpMethod.GET,
-                            null,
-                            MovieRatingEntity[].class
-                    );
-            if (!responseEntity.getStatusCode().is2xxSuccessful())
-                throw new Exception("Fetching Rating information from Rating Service has failed");
+//            ResponseEntity<MovieRatingEntity[]> responseEntity =
+//                    restTemplate.exchange(
+//                            "http://localhost:3003/rating/user/" + userId,
+//                            HttpMethod.GET,
+//                            null,
+//                            MovieRatingEntity[].class
+//                    );
+//            if (!userMovieRating .getStatusCode().is2xxSuccessful())
+//                throw new Exception("Fetching Rating information from Rating Service has failed");
+            UserMovieRatingEntity userMovieRating = restTemplate
+                    .getForObject("http://localhost:3003/rating/user/" + userId, UserMovieRatingEntity.class);
 
-            movieRatingList = Arrays.stream(responseEntity.getBody()).toList();
+            movieRatingList = userMovieRating.ratings();
 
             List<MovieCatalogEntity> catalogEntities = movieRatingList.stream().map(rating -> {
-//                MovieDetailEntity movieDetail = restTemplate.getForObject(
-//                        "http://localhost:3002/detail/"+rating.getMovieId(),
-//                        MovieDetailEntity.class
-//                );
-                MovieDetailEntity movieDetail = webclientBuilder.build()
-                        .get()
-                        .uri("http://localhost:3002/detail/"+rating.getMovieId())
-                        .retrieve()
-                        .bodyToMono(MovieDetailEntity.class)
-                        .block();
-
+                MovieDetailEntity movieDetail = restTemplate.getForObject(
+                        "http://localhost:3002/detail/"+rating.getMovieId(),
+                        MovieDetailEntity.class
+                );
                 return new MovieCatalogEntity(
                         movieDetail.getName(),
                         movieDetail.getDescription(),
@@ -67,8 +67,9 @@ public class MovieCatalogController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    */
 
+//    USING WEB CLIENT FOR API CALLS TO OTHER SERVICES
+    /*
     @GetMapping("/{userId}")
     public ResponseEntity<List<MovieCatalogEntity>> getMovieUserCatalog(@PathVariable String userId) {
         try{
@@ -105,4 +106,6 @@ public class MovieCatalogController {
             return ResponseEntity.internalServerError().body(null);
         }
     }
+
+     */
 }
